0585ab677f6cb9d046caae90585d1e2289fba57a
Vinci-da-Gama <11582145@student.uts.edu.au>
2017-09-23 07:44:59 +1000

    Start Modules

---
+++ eCommerce.Module.MoneyOff/eVoucherMoneyOff.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eCommerce.Contracts.iModels;
+using eCommerce.Contracts.Repos;
+
+namespace eCommerce.Module.MoneyOff
+{
+    public class eVoucherMoneyOff
+    {
+    }
+}
